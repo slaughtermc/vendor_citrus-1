0497786dba9ec5ab64a505eb95df0a3bee9d8f9b
slaughtermc <47281879+slaughtermc@users.noreply.github.com>
2019-02-06 10:46:48 +0530

    Add files via upload

---
+++ config/common_full_phone.mk
@@ -0,0 +1,24 @@
+# Telephony
+
+IS_PHONE := true
+
+# World APN list
+PRODUCT_PACKAGES += \
+    apns-conf.xml
+
+# Telephony packages
+PRODUCT_PACKAGES += \
+    Stk \
+    CellBroadcastReceiver
+
+# Tethering - allow without requiring a provisioning app
+# (for devices that check this)
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    net.tethering.noprovisioning=true
+
+# Default ringtone
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.config.ringtone=The_big_adventure.ogg
+
+# Inherit full common PixelExperience stuff
+$(call inherit-product, vendor/citrus/config/common_full.mk)
